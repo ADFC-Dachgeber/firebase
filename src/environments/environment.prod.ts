faa5279a7a5010ad004fa866a45a217ac573cbd0
Taro Murao <taro.murao@gmail.com>
2021-12-22 12:32:13 +0100

    installed @angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'adfc-staging',
+    appId: '1:714822540529:web:88207784196dae0c745d50',
+    databaseURL: 'https://adfc-staging-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'adfc-staging.appspot.com',
+    locationId: 'europe-west3',
+    apiKey: 'AIzaSyDvFWeSCcxw4oMLw2nkY91ARtizGSLJRVU',
+    authDomain: 'adfc-staging.firebaseapp.com',
+    messagingSenderId: '714822540529',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'adfc-staging',
+    appId: '1:714822540529:web:88207784196dae0c745d50',
+    databaseURL: 'https://adfc-staging-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'adfc-staging.appspot.com',
+    locationId: 'europe-west3',
+    apiKey: 'AIzaSyDvFWeSCcxw4oMLw2nkY91ARtizGSLJRVU',
+    authDomain: 'adfc-staging.firebaseapp.com',
+    messagingSenderId: '714822540529',
+  },
   production: false
 };
 
