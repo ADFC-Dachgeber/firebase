555140fb5a3ea8a762f6d08a77e9ee7a43310147
Taro Murao <taro.murao@gmail.com>
2021-12-30 21:10:01 +0100

    Created Dachgeber interface

---
+++ src/app/dachgeber.ts
@@ -0,0 +1,8 @@
+import { Coordinate } from "ol/coordinate";
+
+export interface Dachgeber {
+    uids: ReadonlyArray<string>;
+    names: ReadonlyArray<string>;
+    coordinate: Coordinate;
+    description: string;
+}
